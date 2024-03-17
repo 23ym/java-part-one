93ae27dddaabd3eb5a43885c81a5e265a4a7e7e7
23ym <zoobazzar@gmail.com>
2024-03-17 13:12:25 +0200

    myFamily

---
+++ src/MyFamily.java
@@ -0,0 +1,7 @@
+public class MyFamily {
+    public static void main(String[] args) {
+        System.out.println("John");
+        System.out.println("Jessica");
+        System.out.println("Сніжана");
+    }
+}
