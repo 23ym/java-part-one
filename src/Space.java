b4d6b0bffd410e09ff31fc184bf42e4bcb06f5de
23ym <zoobazzar@gmail.com>
2024-03-19 01:41:28 +0200

    Java Space

---
+++ src/Space.java
@@ -0,0 +1,15 @@
+public class Space {
+    public static void main(String[] args) {
+        int[] dayOfMonth = {31,28,31,30,31,30,31,31,30,31,30,31};
+        for (int i : dayOfMonth) {
+            System.out.println(i);
+        }
+        String s = "Hello, world!";
+        System.out.println(s);
+
+        String name = "Ivan";
+        int age = 36;
+        String result = "Hi, " + name + "! This is " + age + " years.";
+        System.out.println(result);
+    }
+}
